da03757ed716d9ac56288ed22cd933240aad4c04
garrett04 <garrettcorreia21@gmail.com>
2023-11-18 20:14:42 +0000

    Update script.js

--- PortfolioProjects/Mixed Messages/script.js
+++ PortfolioProjects/Mixed Messages/script.js
@@ -1,8 +1,8 @@
-inspirationalMessages = ['There is always light. If only we\'re brave enough to see it. If only we\'re brave enough to be it.', 'You\'re braver than you believe, stronger than you seem, and smarter than you think.', 'Believe you can and you\'re halfway there.'];
-nonsensicalJokes = ['How do you make an octopus laugh? You give him ten tickles.', 'What does a house wear? Address.', 'Did you hear the rumor about butter? Never mind, I shouldn\'t spread it.'];
-puns = ['The guy who invented the door knocker got a no-bell prize.', 'His theory on inertia never seemed to gain momentum.', 'The dead batteries were given out free of charge.'];
-
-let messages = [inspirationalMessages, nonsensicalJokes, puns] // An array that nests all the messages
+const messages = {
+  inspirationalMessages: ['There is always light. If only we\'re brave enough to see it. If only we\'re brave enough to be it.', 'You\'re braver than you believe, stronger than you seem, and smarter than you think.', 'Believe you can and you\'re halfway there.'],
+  nonsensicalJokes: ['How do you make an octopus laugh? You give him ten tickles.', 'What does a house wear? Address.', 'Did you hear the rumor about butter? Never mind, I shouldn\'t spread it.'],
+  puns: ['The guy who invented the door knocker got a no-bell prize.', 'His theory on inertia never seemed to gain momentum.', 'The dead batteries were given out free of charge.']
+}
 
 // A function that picks a random number
 const randomPicker = num => {
@@ -11,8 +11,8 @@ const randomPicker = num => {
 
 let messagesArr = [];
 
-for (let i = 0; i < messages.length; i++) {
-  let index = randomPicker(messages.length);
+for (let message in messages) {
+  let index = randomPicker(messages[message].length);
   
   // Using if/else if/else
   /*
@@ -26,17 +26,16 @@ for (let i = 0; i < messages.length; i++) {
   */
   
   // Using switch case
-  switch (i) {
-    case 0:
-      messagesArr.push(`Inspirational quote for the day: ${messages[0][index]}`);
+  switch (message) {
+    case 'inspirationalMessages':
+      messagesArr.push(`Inspirational quote for the day: ${messages[message][index]}`);
       break;
-    case 1:
-      messagesArr.push(`Nonsensical joke for the day: ${messages[1][index]}`);
+    case 'nonsensicalJokes':
+      messagesArr.push(`Nonsensical joke for the day: ${messages[message][index]}`);
       break;
-    case 2:
-      messagesArr.push(`Pun for the day: ${messages[2][index]}`);
+    case 'puns':
+      messagesArr.push(`Pun for the day: ${messages[message][index]}`);
       break;
-      
   }
 }
 
