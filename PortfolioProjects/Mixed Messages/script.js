096a94c3206308dc5620b192eb0906f946d814c8
garrett04 <garrettcorreia21@gmail.com>
2023-11-18 18:56:28 +0000

    Mixed messages idea #4

--- PortfolioProjects/Mixed Messages/script.js
+++ PortfolioProjects/Mixed Messages/script.js
@@ -2,24 +2,42 @@ inspirationalMessages = ['There is always light. If only we\'re brave enough to 
 nonsensicalJokes = ['How do you make an octopus laugh? You give him ten tickles.', 'What does a house wear? Address.', 'Did you hear the rumor about butter? Never mind, I shouldn\'t spread it.'];
 puns = ['The guy who invented the door knocker got a no-bell prize.', 'His theory on inertia never seemed to gain momentum.', 'The dead batteries were given out free of charge.'];
 
-let messages = [inspirationalMessages, nonsensicalJokes, puns]
+let messages = [inspirationalMessages, nonsensicalJokes, puns] // An array that nests all the messages
 
+// A function that picks a random number
 const randomPicker = num => {
   return Math.floor(Math.random() * num);
-}
+} 
 
-let pickMessage = 0;
-let messageArr = [];
+let messagesArr = [];
 
 for (let i = 0; i < messages.length; i++) {
-  pickMessage = randomPicker(messages.length);
+  let index = randomPicker(messages.length);
   
+  // Using if/else if/else
+  /*
   if (i === 0) {
-    messageArr.push(`Inspirational quote for the day: ${messages[0][pickMessage]}`);
+    messagesArr.push(`Inspirational quote for the day: ${messages[0][pickMessage]}`);
   } else if (i === 1) {
-    messageArr.push(`Pun for the day: ${messages[1][pickMessage]}`);
+    messagesArr.push(`Nonsensical joke for the day: ${messages[1][pickMessage]}`);
+  } else {
+    messagesArr.push(`Pun for the day: ${messages[2][pickMessage]}`);
+  }
+  */
+  
+  // Using switch case
+  switch (i) {
+    case 0:
+      messagesArr.push(`Inspirational quote for the day: ${messages[0][index]}`);
+      break;
+    case 1:
+      messagesArr.push(`Nonsensical joke for the day: ${messages[1][index]}`);
+      break;
+    case 2:
+      messagesArr.push(`Pun for the day: ${messages[2][index]}`);
   }
 }
 
+let messagesStr = messagesArr.join('\n\n'); // Storing all messages as a string in messagesStr.
 
-console.log(messageArr.join('\n'));
+console.log(messagesStr);
