5474f593e86c1b7759772493a174f950fcb30fff
garrett04 <garrettcorreia21@gmail.com>
2023-11-22 00:58:20 +0000

    Added mobilestyles.css

--- PortfolioProjects/Portfolio Website/resources/script.js
+++ PortfolioProjects/Portfolio Website/resources/script.js
@@ -6,11 +6,9 @@ let grayscaleImage = "url(./resources/images/background-image-grayscale.png)";
 function colorSwitch() {
     if (toggleButton.innerHTML === 'Dark Theme') {
         bodyImage.backgroundImage = '';
-        bodyImage.color = '';
         toggleButton.innerHTML = 'Light Theme';
     } else {
         bodyImage.backgroundImage = grayscaleImage;
-        bodyImage.color = 'white';
         toggleButton.innerHTML = 'Dark Theme';
     }
 }
