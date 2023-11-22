a241b7f1d0c3cc78614a2291354f5a1035d5c7af
garrett04 <garrettcorreia21@gmail.com>
2023-11-22 10:38:01 +0000

    Update all

--- PortfolioProjects/Portfolio Website/resources/script.js
+++ PortfolioProjects/Portfolio Website/resources/script.js
@@ -1,4 +1,5 @@
 let bodyImage = document.body.style;
+let headerColor = document.getElementsByTagName('header')[0].style;
 let toggleButton = document.getElementsByTagName('button')[0];
 let grayscaleImage = "url(./resources/images/background-image-grayscale.png)";
 
@@ -6,9 +7,11 @@ let grayscaleImage = "url(./resources/images/background-image-grayscale.png)";
 function colorSwitch() {
     if (toggleButton.innerHTML === 'Dark Theme') {
         bodyImage.backgroundImage = '';
+        headerColor.backgroundColor = '';
         toggleButton.innerHTML = 'Light Theme';
     } else {
         bodyImage.backgroundImage = grayscaleImage;
+        headerColor.backgroundColor = 'black';
         toggleButton.innerHTML = 'Dark Theme';
     }
 }
