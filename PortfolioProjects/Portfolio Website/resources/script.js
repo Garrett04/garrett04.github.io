69c933286e33ea509df24dba25283ab29ffdffbe
garrett04 <garrettcorreia21@gmail.com>
2023-11-24 17:52:17 +0000

    Update script.js

--- PortfolioProjects/Portfolio Website/resources/script.js
+++ PortfolioProjects/Portfolio Website/resources/script.js
@@ -38,3 +38,22 @@ function colorSwitchMobile() {
         toggleButtonMobile.innerHTML = 'Dark Theme';
     }
 }
+
+/* Prototype
+function addImage(event) {
+    if (event.target.innerHTML === 'Responsive Club Website') {
+        const desktopViewImg = '<img src="./resources/images/desktop-view.png" style="display: inline-flex; width: 40rem; margin-bottom: 12rem; border-radius: 1rem;">';
+        const mobileViewImg = '<img src="./resources/images/mobile-view.jpeg" style="display: inline; width: 20rem; height: auto; border-radius: 1rem;">';
+        event.target.insertAdjacentHTML('afterend', '<br>' + desktopViewImg + mobileViewImg);
+        event.target.removeEventListener('mouseover', addImage);
+    }
+}
+
+const project1 = document.getElementsByTagName('a')[5];
+const project2 = document.getElementsByTagName('a')[6];
+
+
+project1.addEventListener('mouseover', addImage);
+
+console.log(document.getElementsByTagName('li')[4].children);
+*/
