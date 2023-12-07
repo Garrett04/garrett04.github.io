0bff1558fc814c0cd203b42799eb2e400d71629b
garrett04 <garrettcorreia21@gmail.com>
2023-12-07 14:21:08 +0000

    Step4 Completed Update ContactForm.js

--- CodecademyProjects/Front-End Development/appointment_planner/src/components/contactForm/ContactForm.js
+++ CodecademyProjects/Front-End Development/appointment_planner/src/components/contactForm/ContactForm.js
@@ -11,8 +11,11 @@ export const ContactForm = ({
 }) => {
   return (
     <>
-      <form>
-
+      <form onSubmit={handleSubmit}>
+        <input name={name} onChange={({target}) => setName(target.value)} required />
+        <input name={phone} type="tel" pattern="^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$" onChange={({target}) => setPhone(target.value)} required />
+        <input name={email} type="email" onChange={({target}) => setEmail(target.value)} required />
+        <input type='submit'/>
       </form>
     </>
   );
