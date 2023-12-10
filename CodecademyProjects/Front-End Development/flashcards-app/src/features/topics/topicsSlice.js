f3978c500096f97b3acddeca30a528b0d1e152eb
garrett04 <garrettcorreia21@gmail.com>
2023-12-10 16:14:22 +0000

    Step8 Completed

--- CodecademyProjects/Front-End Development/flashcards-app/src/features/topics/topicsSlice.js
+++ CodecademyProjects/Front-End Development/flashcards-app/src/features/topics/topicsSlice.js
@@ -1,33 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    topics: {
-        '123': {
-            id: '123',
-            name: 'example topic',
-            icon: 'icon url'
-        },
-        '234': {
-            id: '234',
-            name: 'example topic 2',
-            icon: 'icon url 2'
-        }
-    }
+    topics: {},
 }
 
 const topicsSlice = createSlice({
     name: 'topics',
-    initialState: initialState,
+    initialState,
     reducers: {
         addTopic: {
             reducer(state, action) {
                 const { id, name, icon } = action.payload;
-                state[id] = action.payload;
+                state.topics[id] = {
+                    id, 
+                    name, 
+                    icon, 
+                    quizIds: [] 
+                }; 
+            },
+            prepare(id, name, icon) {
+                return {
+                    payload: {
+                        id,
+                        name,
+                        icon,
+                        quizIds: []
+                    }
+                }
             }
         }
     }
 })
 
-export const selectAllTopics = (state) => state.topics;
+export const selectAllTopics = (state) => state.topics.topics;
 export const { addTopic } = topicsSlice.actions;
 export default topicsSlice.reducer;
